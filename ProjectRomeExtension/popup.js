473f8d6cb0bce5ed6227130ff39584891b39e6f2
Shawn Henry <shahen@microsoft.com>
2017-12-21 15:04:06 -0800

    Code cleanup

--- ProjectRomeExtension/popup.js
+++ ProjectRomeExtension/popup.js
@@ -1,74 +1,66 @@
 
-	function buildDeviceTable(obj) {
-		var select = document.getElementById("devicelist");
-		select.length = 0;
-		for (var i = 0; i < obj.length; i++) {
-			select.options[select.length] =
-				new Option(obj[i]["Name"], obj[i]["id"]);
-		}
-		select.style.display = "block";
+function buildDeviceTable(obj) {
+	var select = document.getElementById("devicelist");
+	select.length = 0;
+	for (var i = 0; i < obj.length; i++) {
+		select.options[select.length] =
+			new Option(obj[i]["Name"], obj[i]["id"]);
 	}
+	select.style.display = "block";
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('launchButton').addEventListener('click', function () {
+		var e = document.getElementById("devicelist");
+		var deviceId = e.options[e.selectedIndex].value;
 
+		var tabUrl = 'http://bing.com'; //default uri
+		chrome.tabs.getSelected(null, function (tab) {
+			tabUrl = tab.url;
 
-	document.addEventListener('DOMContentLoaded', function () {
-
-		var launchButton = document.getElementById('launchButton');
-
-		launchButton.addEventListener('click', function () {
-			var e = document.getElementById("devicelist");
-			var deviceId = e.options[e.selectedIndex].value;
-
-			var tabUrl = 'http://bing.com';
-			chrome.tabs.getSelected(null, function (tab) {
-				tabUrl = tab.url;
-
-				launchUri(deviceId, tabUrl, (err, res) => {
-					if (err) {
-						console.log(err);
-						return;
-					}
-					console.log(res);
-				});
-			});
-
-
-		}, false);
-
-    		var activityButton = document.getElementById('activityButton');
-
-		activityButton.addEventListener('click', function () {
-			var tabUrl = 'http://bing.com';
-			chrome.tabs.getSelected(null, function (tab) {
-				tabUrl = tab.url;
-				var tabTitle = tab.title;
-
-				if (!tabTitle) {
-					tabTitle = tabUrl;
+			launchUri(deviceId, tabUrl, (err, res) => {
+				if (err) {
+					console.log(err);
+					return;
 				}
+				console.log(res);
+			});
+		});
 
-				createActivity(tabUrl, tabTitle, (err, res) => {
+	document.getElementById('activityButton').addEventListener('click', function () {
+		var tabUrl = 'http://bing.com';
+		chrome.tabs.getSelected(null, function (tab) {
+			tabUrl = tab.url;
+			var tabTitle = tab.title;
+
+			if (!tabTitle) {
+				tabTitle = tabUrl;
+			}
+
+			createActivity(tabUrl, tabTitle, (err, res) => {
+				if (err) {
+					console.log(err);
+					return;
+				}
+				console.log(res);
+				createEngagement(res.header.location, (err, res) => {
 					if (err) {
 						console.log(err);
-						
 						return;
 					}
 					console.log(res);
-					createEngagement(res.header.location, (err, res) => {
-						if (err) {
-							console.log(err);
-							return;
-						}
-						console.log(res);
-					});
 				});
 			});
-		}, false);
+		});
+	}, false);
+}, false);
 
-		login().then(function(response) {
-			console.log("Success!", response);
-			getGraph();
-		  }, function(error) {
-			console.error("Failed!", error);
-		  })
-	});
+
+
+login().then(function(response) {
+	console.log("Success!", response);
+	getDeviceGraph();
+	}, function(error) {
+	console.error("Failed!", error);
+	})
+});
